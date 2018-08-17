5e92e7f898d8bb79e5df74a39224e104d1991619
Lê Mạnh Niên <nienbkict@gmail.com>
2018-08-17 18:04:07 +0700

    test deploy gate ?

--- app/src/main/java/com/midsummer/sample/MainActivity.kt
+++ app/src/main/java/com/midsummer/sample/MainActivity.kt
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.midsummer.w3jl.entity.W3JLTokenInfo
+import com.midsummer.w3jl.entity.W3JLWallet
 import com.midsummer.w3jl.service.W3JLFactory
 import com.midsummer.w3jl.util.BalanceUtil
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.math.BigInteger
 import java.util.concurrent.TimeUnit
@@ -34,6 +36,7 @@ class MainActivity : AppCompatActivity() {
 
 
         val w3JL = W3JLFactory()
+                .withContext(this)
                 .buildW3JLWallet()
 
         /*val kethe = TestKethe()
@@ -46,19 +49,17 @@ class MainActivity : AppCompatActivity() {
         compositeDisposable.add(w3JL.createWalletFromMnemonic(mnemonic, "niendeptrai")
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .delay(5, TimeUnit.SECONDS)
-                .subscribe({
-                    Log.d(TAG,"wallet: $it")
-                },{
-                    Log.d(TAG,"wallet Error: ${Log.getStackTraceString(it)}")
-                }))
+                .doOnSubscribe {  displayWallet("loading") }
+                .doOnSuccess { wallet -> displayWallet(wallet.toString()) }
+                .doOnError { e -> displayWallet(e.localizedMessage) }
+                .subscribe())
 
         val w3JLEth = W3JLFactory()
                 .withContext(this)
                 .withNetworkProvider("https://ropsten.infura.io/v3/95fa3a86534344ee9d1bf00e2b0d6d06")
                 .buildW3JLEth()
 
-        compositeDisposable.add(w3JLEth.getAccountBalance(addressFrom, 5)
+        /*compositeDisposable.add(w3JLEth.getAccountBalance(addressFrom, 5)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .delay(5, TimeUnit.SECONDS)
@@ -66,25 +67,25 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG,"balance: $it")
                 },{
                     Log.d(TAG,"balance Error: ${it.localizedMessage}")
-                }))
+                }))*/
 
 
-        val tokenInfo = W3JLTokenInfo()
+        /*val tokenInfo = W3JLTokenInfo()
         tokenInfo.address = "0x86a85ce4CC3FA15a4C24Ae720990d998A65B4917"
         tokenInfo.name = "Venus"
         tokenInfo.symbol = "VNS"
-        tokenInfo.decimals = 18
+        tokenInfo.decimals = 18*/
 
-        val tokenOwnerAddress =  "0x95b52bcd93D4A87a7E98975F2245a57789a2D34d"
+        /*val tokenOwnerAddress =  "0x95b52bcd93D4A87a7E98975F2245a57789a2D34d"
         val tokenOwnerPrivateKey = "5DF2D1B7F71A6B2FF6F6B5B72EC8FFDF49E0DD4EF65390E8923AC0F0FC1F1371"
         val tokenTo = "0xB7983Ce7796d47616CE896b2d006145BC856Dc0E"
         val tokenRepository = W3JLFactory()
                 .withContext(this)
                 .withNetworkProvider("https://ropsten.infura.io/v3/95fa3a86534344ee9d1bf00e2b0d6d06")
                 .withTokenInfo(tokenInfo)
-                .buildW3JLToken()
+                .buildW3JLToken()*/
 
-        compositeDisposable.add(tokenRepository.getBalance(tokenOwnerAddress, tokenInfo)
+        /*compositeDisposable.add(tokenRepository.getBalance(tokenOwnerAddress, tokenInfo)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .delay(5, TimeUnit.SECONDS)
@@ -92,7 +93,7 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG,"balance: ${BalanceUtil.weiToEth(it, 3)}")
                 },{
                     Log.d(TAG,"balance Error: ${it.localizedMessage}")
-                }))
+                }))*/
 
 
         /*compositeDisposable.add(tokenRepository.transferToken(tokenOwnerAddress, tokenTo,  tokenOwnerPrivateKey,
@@ -113,6 +114,12 @@ class MainActivity : AppCompatActivity() {
                 tokenInfo.address,BalanceUtil.baseToSubunit(111, 18))
         Log.d(TAG, "txId: $txId")*/
     }
+
+
+    private fun displayWallet(result: String) {
+        txtHome.text = result
+    }
+
 
     public override fun onDestroy() {
         super.onDestroy()
